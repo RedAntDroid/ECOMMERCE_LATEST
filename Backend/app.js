2f55669dc734aea33100d57f6cffcc9aa58a6e81
Vigneshwaran-star <nanbanenemy143@gmail.com>
2025-01-30 10:05:07 +0530

    milestone6

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const ErrorHandler = require('./utlis/errorHandler'); 
+const ErrorHandler = require('./utils/ErrorHandler'); 
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors")
