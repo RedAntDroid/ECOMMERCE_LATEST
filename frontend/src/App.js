711453061f0457f16d63736f2da18ddda4875e09
Vigneshwaran-star <nanbanenemy143@gmail.com>
2025-01-31 15:35:15 +0530

    milstone-8

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from './pages/LoginPage';  // Import directly
 import SignUpPage from './pages/SignUpPage';
 import "./App.css";
+import Home from './pages/Home'; 
 
 const App = () => {
   return (
     <BrowserRouter>
+   
       <Routes>
+        <Route path="/" element={<Home/>} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+       
       </Routes>
     </BrowserRouter>
   );
